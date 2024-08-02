8820c2bb9fb501f5139b1a8ad0e05d8bdc2ee02c
myq1991 <myq1991@gmail.com>
2024-08-02 11:45:02 +0800

    fix(cli): remove commented code in OnlineLatestVersion.ts
    This commit removes commented out code in the OnlineLatestVersion.ts file in the packages/cli/src/lib/providers directory.

--- packages/cli/src/lib/providers/OnlineLatestVersion.ts
+++ packages/cli/src/lib/providers/OnlineLatestVersion.ts
@@ -25,10 +25,8 @@ export class OnlineLatestVersion extends Provider {
         const prereleaseInfo: ReadonlyArray<string | number> | null = prerelease(this.version)
         const latestVersionPackage = await import('latest-version')
         if (prereleaseInfo && prereleaseInfo[0]) {
-            // onlineLatestVersion = await require('latest-version').default(this.name, {version: prereleaseInfo[0].toString()})
             onlineLatestVersion = await latestVersionPackage.default(this.name, {version: prereleaseInfo[0].toString()})
         } else {
-            // onlineLatestVersion = await require('latest-version').default(this.name)
             onlineLatestVersion = await latestVersionPackage.default(this.name)
         }
         return isVersionGreaterThan(onlineLatestVersion, this.version) ? onlineLatestVersion : this.version
