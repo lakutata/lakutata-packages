503a8af2c748b2bc5f5090e44a202f04f5363baf
myq1991 <myq1991@gmail.com>
2024-08-02 11:42:01 +0800

    feat: Add import statement for 'latest-version' package in OnlineLatestVersion.ts
    The commit adds an import statement for the 'latest-version' package in the OnlineLatestVersion.ts file. This allows the package to be used to retrieve the latest version of the package.

--- packages/cli/src/lib/providers/OnlineLatestVersion.ts
+++ packages/cli/src/lib/providers/OnlineLatestVersion.ts
@@ -23,10 +23,13 @@ export class OnlineLatestVersion extends Provider {
     public async getVersion(): Promise<string> {
         let onlineLatestVersion: string
         const prereleaseInfo: ReadonlyArray<string | number> | null = prerelease(this.version)
+        const latestVersionPackage = await import('latest-version')
         if (prereleaseInfo && prereleaseInfo[0]) {
-            onlineLatestVersion = await require('latest-version').default(this.name, {version: prereleaseInfo[0].toString()})
+            // onlineLatestVersion = await require('latest-version').default(this.name, {version: prereleaseInfo[0].toString()})
+            onlineLatestVersion = await latestVersionPackage.default(this.name, {version: prereleaseInfo[0].toString()})
         } else {
-            onlineLatestVersion = await require('latest-version').default(this.name)
+            // onlineLatestVersion = await require('latest-version').default(this.name)
+            onlineLatestVersion = await latestVersionPackage.default(this.name)
         }
         return isVersionGreaterThan(onlineLatestVersion, this.version) ? onlineLatestVersion : this.version
     }
